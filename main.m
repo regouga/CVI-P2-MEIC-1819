c61abac2dd0b5bf2d6fa3188356740ff34d202f4
André Pereira <andrespereira@tecnico.ulisboa.pt>
2019-06-02 15:15:45 +0100

    trajectories done?

--- main.m
+++ main.m
@@ -1,26 +1,38 @@
 clear all, close all
 
-totalFrames = 795;
+totalF = 795;
+trainF = 200;
 step = 1;
 currentFrame = 1;
 k = 1;
 threshold = 50;
 previous_bw = zeros([576 768]);
 alfa = 0.01; % Background computation
+path = '3DMOT2015/train/PETS09-S2L1/img1/';
+str2 = ['%s%.' num2str(6) 'd.%s'];
 
+frameNumb = 1;
 
-vid3D = zeros([576 768 totalFrames/step]);
+vid3D = zeros([576 768 trainF/step]);
 bkg = zeros(576, 768);
 
 
-for i = 1 : step : 500
+for i = 1 : step : totalF
         img = imread(sprintf('3DMOT2015/train/PETS09-S2L1/img1/%.6d.jpg',i));
         vid3D(:,:,k) = rgb2gray(img);
         bkg = alfa * double(vid3D(:,:,k)) + (1-alfa) * double(bkg);
         k = k + 1;
+        % NOTE1: loop part of code that contributed to removal of people
+        % and leaves the background only, useful for trajectory drawing?
+        %vid4D(:,:,:,i) = img;
+        %imshow(img);
 end
+%Background only, see NOTE1 above
+%bkg = median(vid4D,4);
+%figure;imagesc(uint8(bkg));
 
-for i = 1 : step : totalFrames
+
+for i = 1 : step : trainF
         img = imread(sprintf('3DMOT2015/train/PETS09-S2L1/img1/%.6d.jpg',i));
         
         vid3D(:,:,k) = rgb2gray(img);
@@ -57,5 +69,69 @@ for i = 1 : step : totalFrames
         drawnow;
         hold off;
         prev_num =  num;
+        k = k + 1;
+end
+
+for i = trainF : step : totalF
+        img = imread(sprintf(str2,path,i,'jpg'));
+
+        vid3D(:,:,k) = rgb2gray(img);
+
+        bw = (abs(vid3D(:,:,k) - bkg) > threshold);
+        bw_final = bwareaopen(bw, 100);
+        bw_final = bwmorph(bw_final,'close');
+        se = strel('disk', 2);
+        bw_final = imdilate(bw_final,se);
+        se = strel('disk', 5);
+        bw_final = imclose(bw_final,se);
+        bw_final = bwareaopen(bw_final, 350);
+        bw_image = (bw_final + previous_bw) > 0;
+        previous_bw = bw_final; 
+        
+        [lb, num]= bwlabel(bw_image);
+        stats = regionprops(lb);
+        objects = [stats.Area];
+        
+        centroids = zeros(length(objects), 2); % To save (sum of lines, sum of columns) for each label
+        for a = 1 : size(lb,1) % For each line
+            for j = 1 : size(lb,2) % For each column
+                if lb(a,j) ~= 0 % If it's not background
+                    centroids(lb(a,j),1) = centroids(lb(a,j),1) + a; % Sum the lines
+                    centroids(lb(a,j),2) = centroids(lb(a,j),2) + j; % Sum the columns
+                end
+            end
+        end
+
+        for l = 1 : length(objects) % For each object
+            centroids(l,1) = centroids(l,1)/objects(l); % lines' = sum(lines)/area
+            centroids(l,2) = centroids(l,2)/objects(l); % columns' = sum(columns)/area
+            pathing(1, frameNumb, l) = centroids(l,1);
+            pathing(2, frameNumb, l) = centroids(l,2);
+        end
+
+        %imshow(img); hold on;
+        %takes image and returns a gray version. Colours are then used for
+        %trajectory "breadcrumb" trails
+        imagesc(uint8(vid3D(:, :, k))); colormap gray; hold on;
+        axis off;
+        
+        %trajectory plotted on the image
+        for a = 1 : num
+            x_plot = [];
+            y_plot = [];
+            for j = 1 : frameNumb
+                x_plot = [ x_plot pathing(1, j, a) ];
+                y_plot = [ y_plot pathing(2, j, a) ];
+            end
+            if(stats(a).BoundingBox(3) / stats(a).BoundingBox(4) > 1)
+                plot(y_plot, x_plot, 'r.', 'MarkerSize', 5);
+            else
+                plot(y_plot, x_plot, 'b.', 'MarkerSize', 5);
+            end
+            %drawn result
+            drawnow;
+        end
+        hold off;
+        frameNumb = frameNumb + 1;
         k = k + 1;
 end
